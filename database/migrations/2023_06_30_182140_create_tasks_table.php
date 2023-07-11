026e18ac27a648077e800c69a23b2a0f33244571
Big Baddie <coems_isreal@gmail.com>
2023-07-11 20:26:49 +0300

    New file

---
+++ database/migrations/2023_06_30_182140_create_tasks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new      class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); // Название задачи
+            $table->string('preview'); // Краткий текст задачи
+            $table->text('detail'); // Полный текст задачи
+            $table->string('file'); // Вложение
+            $table->boolean('priority')->default(false); // Высокий приоритет
+            $table->foreignId('status_id')->references('id')->on('tasks');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
